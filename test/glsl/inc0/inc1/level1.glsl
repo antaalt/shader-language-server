50f84cc7ca3c6819c4b5a9b5dc88be32639c9760
Antoine <aaltorffer@orange.fr>
2024-08-23 01:11:36 +0200

    Add function in test

--- test/glsl/inc0/inc1/level1.glsl
+++ test/glsl/inc0/inc1/level1.glsl
@@ -1,2 +1,12 @@
 
 const float level1 = 43.0;
+
+uint fibonacciLevel1(uint nthNumber) {
+    int prevprevNumber, prevNumber = 0, currentNumber = 1;
+    for (int i = 1; i < nthNumber ; i++) {
+        prevprevNumber = prevNumber;
+        prevNumber = currentNumber;
+        currentNumber = prevprevNumber + prevNumber;
+    }
+    return currentNumber;
+}
--- test/glsl/inc0/level0.glsl
+++ test/glsl/inc0/level0.glsl
@@ -2,10 +2,8 @@
 
 const float level0 = level1 + 4.0;
 
-uint fibonacci(uint nthNumber) {
-    //use loop
+uint fibonacciLevel0(uint nthNumber) {
     int prevprevNumber, prevNumber = 0, currentNumber = 1;
-
     for (int i = 1; i < nthNumber ; i++) {
         prevprevNumber = prevNumber;
         prevNumber = currentNumber;

4a858f73cf6de021743bebf5b30765243dd0f506
Antoine <aaltorffer@orange.fr>
2024-08-26 19:17:41 +0200

    Fix crash hlsl / glsl & add test to avoid this

--- src/shaders/symbols/mod.rs
+++ src/shaders/symbols/mod.rs
@@ -1,2 +1,26 @@
 mod glsl;
 pub mod symbols;
+
+
+#[cfg(test)]
+mod tests {
+    use crate::shaders::shader::ShadingLanguage;
+
+    use super::symbols::get_default_shader_completion;
+
+    #[test]
+    fn intrinsics_glsl_ok() {
+        // Ensure parsing of intrinsics is OK
+        let _ = get_default_shader_completion(ShadingLanguage::Glsl);
+    }
+    #[test]
+    fn intrinsics_hlsl_ok() {
+        // Ensure parsing of intrinsics is OK
+        let _ = get_default_shader_completion(ShadingLanguage::Hlsl);
+    }
+    #[test]
+    fn intrinsics_wgsl_ok() {
+        // Ensure parsing of intrinsics is OK
+        let _ = get_default_shader_completion(ShadingLanguage::Wgsl);
+    }
+}

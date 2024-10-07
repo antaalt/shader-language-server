39fb578129004159815fb8aaba3f658b3e717eb9
Antoine <aaltorffer@orange.fr>
2024-10-07 23:20:05 +0200

    Parse hlsl function in tree

--- src/shaders/symbols/hlsl_parser.rs
+++ src/shaders/symbols/hlsl_parser.rs
@@ -1,61 +1,67 @@
-use crate::shaders::shader::ShaderStage;
+use std::path::Path;
 
-use super::symbols::{ShaderSymbolList, SymbolFilter};
+use super::{parser::{get_name, SymbolTreeParser}, symbols::{ShaderParameter, ShaderRange, ShaderScope, ShaderSignature, ShaderSymbol, ShaderSymbolData, ShaderSymbolList, SymbolFilter}};
 
-pub struct HlslVersionFilter {}
+pub(super) struct HlslFunctionTreeParser {}
 
-impl SymbolFilter for HlslVersionFilter {
-    fn filter_symbols(&self, _shader_symbols: &mut ShaderSymbolList, _file_name: &String) {
-        // TODO: filter version
+impl SymbolTreeParser for HlslFunctionTreeParser {
+    fn get_query(&self) -> &str {
+        // Ensure we dont pick struct_specifier
+        r#"(function_definition
+            type: [
+                (type_identifier) @function.return
+                (primitive_type) @function.return
+            ]
+            function_declarator: (function_declarator
+                declarator: (identifier) @function.label
+                parameter_list: (parameter_list 
+                    ((parameter_declaration
+                        type_identifier: (_) @function.param.type
+                        declarator: (_) @function.param.decl
+                    )(",")?)*
+                )
+            )
+            body: (compound_statement) @function.scope
+        )"# // compound_statement is function scope.
     }
-}
-pub struct HlslStageFilter {}
-
-impl SymbolFilter for HlslStageFilter {
-    fn filter_symbols(&self, shader_symbols: &mut ShaderSymbolList, file_name: &String) {
-        match ShaderStage::from_file_name(file_name) {
-            Some(shader_stage) => {
-                *shader_symbols = ShaderSymbolList {
-                    types: shader_symbols
-                        .types
-                        .drain(..)
-                        .filter(|value| {
-                            value.stages.contains(&shader_stage) || value.stages.is_empty()
+    fn process_match(&self, matches: tree_sitter::QueryMatch, file_path: &Path, shader_content: &str, scopes: &Vec<ShaderScope>, symbols: &mut ShaderSymbolList) {
+        let label_node = matches.captures[1].node;
+        let range = ShaderRange::from_range(label_node.range(), file_path.into());
+        let scope_stack = self.compute_scope_stack(scopes, &range);
+        // Query internal scopes variables
+        /*let scope_node = matche.captures[matche.captures.len() - 1].node;
+        let content_scope_stack = {
+            let mut s = scope_stack.clone();
+            s.push(range.clone());
+            s
+        };
+        query_variables(file_path, &shader_content[scope_node.range().start_byte.. scope_node.range().end_byte], scope_node, {
+            let mut s = scope_stack.clone();
+            s.push(range.clone());
+            s
+        });*/
+        symbols.functions.push(ShaderSymbol {
+            label: get_name(shader_content, matches.captures[1].node).into(),
+            description: "".into(),
+            version: "".into(),
+            stages: vec![],
+            link: None,
+            data: ShaderSymbolData::Functions {
+                signatures: vec![ShaderSignature {
+                    returnType: get_name(shader_content, matches.captures[0].node).into(),
+                    description: "".into(),
+                    parameters: matches.captures[2..matches.captures.len() - 1]
+                        .chunks(2)
+                        .map(|w| ShaderParameter {
+                            ty: get_name(shader_content, w[0].node).into(),
+                            label: get_name(shader_content, w[1].node).into(),
+                            description: "".into(),
                         })
-                        .collect(),
-                    constants: shader_symbols
-                        .constants
-                        .drain(..)
-                        .filter(|value| {
-                            value.stages.contains(&shader_stage) || value.stages.is_empty()
-                        })
-                        .collect(),
-                    variables: shader_symbols
-                        .variables
-                        .drain(..)
-                        .filter(|value| {
-                            value.stages.contains(&shader_stage) || value.stages.is_empty()
-                        })
-                        .collect(),
-                    functions: shader_symbols
-                        .functions
-                        .drain(..)
-                        .filter(|value| {
-                            value.stages.contains(&shader_stage) || value.stages.is_empty()
-                        })
-                        .collect(),
-                    keywords: shader_symbols
-                        .keywords
-                        .drain(..)
-                        .filter(|value| {
-                            value.stages.contains(&shader_stage) || value.stages.is_empty()
-                        })
-                        .collect(),
-                }
-            }
-            None => {
-                // No filtering
-            }
-        }
+                        .collect::<Vec<ShaderParameter>>(),
+                }],
+            },
+            range: Some(range),
+            scope_stack: Some(scope_stack), // In GLSL, all function are global scope.
+        });
     }
 }

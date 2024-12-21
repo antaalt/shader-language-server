cba13000f8bacf8908623e3013c8f7fe13781066
Antoine <aaltorffer@orange.fr>
2024-12-21 13:02:25 +0100

    Fix fmt

--- shader-sense/examples/main.rs
+++ shader-sense/examples/main.rs
@@ -1,15 +1,25 @@
 use std::path::Path;
 
-use shader_sense::{shader::ShadingLanguage, symbols::create_symbol_provider, validator::{create_validator, validator::ValidationParams}};
+use shader_sense::{
+    shader::ShadingLanguage,
+    symbols::create_symbol_provider,
+    validator::{create_validator, validator::ValidationParams},
+};
 
 fn validate_file(shading_language: ShadingLanguage, shader_path: &Path) {
     // Validator intended to validate a file using standard API.
     let mut validator = create_validator(shading_language);
     let shader_content = std::fs::read_to_string(shader_path).unwrap();
-    match validator.validate_shader(shader_content, shader_path, ValidationParams::default(), &mut |path: &Path| {
-        Some(std::fs::read_to_string(path).unwrap())
-    }) {
-        Ok((diagnostic_list, dependencies)) => println!("Validated file and return following diagnostics: {:#?}\n With dependencies: {:#?}", diagnostic_list, dependencies),
+    match validator.validate_shader(
+        shader_content,
+        shader_path,
+        ValidationParams::default(),
+        &mut |path: &Path| Some(std::fs::read_to_string(path).unwrap()),
+    ) {
+        Ok((diagnostic_list, dependencies)) => println!(
+            "Validated file and return following diagnostics: {:#?}\n With dependencies: {:#?}",
+            diagnostic_list, dependencies
+        ),
         Err(err) => println!("Failed to validate file: {:#?}", err),
     }
 }
@@ -24,7 +34,7 @@ fn query_all_symbol(shading_language: ShadingLanguage, shader_path: &Path) {
                 Ok(symbol_list) => println!("Found symbols: {:#?}", symbol_list),
                 Err(err) => println!("Failed to get all symbols: {:#?}", err),
             }
-        },
+        }
         Err(err) => println!("Failed to create ast: {:#?}", err),
     }
 }
